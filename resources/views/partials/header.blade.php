4fe2e480cb9e78488c7129a5b0427d5ae023dfb1
EnricoClem <enrico.clem94@gmail.com>
2024-05-21 16:10:12 +0200

    ADD - header partial

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="{{ url('/') }}">HOME</a>
+                </li>
+                <li>
+                    <a href="{{ url('/comics') }}">COMICS</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
